ed59a914b541110b48f76ac109acb4f232ab3847
rio197 <rio197@Kedaung-PC>
2018-01-17 21:39:43 -0500

    Initial commit

---
+++ src/iterationalstatements/IterationalStatements.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iterationalstatements;
+
+/**
+ *
+ * @author rio197
+ */
+public class IterationalStatements {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
