52adc5bc706687d96bcef8c3a8ce6dda20b2103f
rio197 <rio197@Kedaung-PC>
2018-01-17 22:25:41 -0500

    Changed the for statement

--- src/iterationalstatements/IterationalStatements.java
+++ src/iterationalstatements/IterationalStatements.java
@@ -16,7 +16,7 @@ public class IterationalStatements {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        for (int i = 1; i <= 5; i = i + 1) {
+        for (int i=1; i<=5; i=i+1) {
             System.out.println("Java");
         }
     }    
